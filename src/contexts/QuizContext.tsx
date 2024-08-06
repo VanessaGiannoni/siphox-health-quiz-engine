1c77bc55f7e22597ae1f0abf5ccc5995fe111109
vanessagiannoni <vanessa.o.giannoni@gmail.com>
2024-08-06 17:59:17 -0300

    refactor: add new functionalities and types on QuizContext, fix Reducer score and current question

--- src/contexts/QuizContext.tsx
+++ src/contexts/QuizContext.tsx
@@ -1,18 +1,16 @@
 import React, { createContext, useContext, useReducer } from 'react';
 
-
 export interface Question {
-  type: "multiple" | "boolean";
+  type: "multiple" | "boolean" | "input" | "one-choice";
   difficulty: "easy" | "medium" | "hard";
   category: string;
   question: string;
-  correct_answer: string;
+  correct_answer: string | string[];
   incorrect_answers: string[];
+  options: string[];
 }
 
-
 export interface QuestionResponse {
-  response_code: number;
   results: Question[];
 }
 
@@ -28,18 +26,24 @@ interface IQuizContext {
 
 type Status = "idle" | "fetching" | "ready" | "error" | "answered";
 
-interface QuizState {
+export interface QuizState {
   gameStatus: Status
   question: Question | null,
-  userAnswer: string | null,
+  userAnswer: string | string[] | null,
   score: Score,
-};
+  currentQuestion: number
+  correctAnswer: string | string[] | null,
+  isCorrect: boolean,
+}
 
-type QuizAction =
+export type QuizAction =
   { type: "setStatus"; payload: Status }
   | { type: "setQuestion"; payload: Question }
-  | { type: "setUserAnswer"; payload: string }
+  | { type: "setUserAnswer"; payload: string | string[] | null }
   | { type: "setScore"; payload: "correct" | "incorrect" }
+  | { type: "setCurrentQuestion"; payload: number }
+  | { type: "setCorrectAnswer"; payload: string | string[] | null }
+  | { type: "setIsCorrect"; payload: boolean }
 
 const initialState: QuizState = {
   gameStatus: "idle",
@@ -48,7 +52,10 @@ const initialState: QuizState = {
   score: {
     correct: 0,
     incorrect: 0
-  }
+  },
+  currentQuestion: 0,
+  correctAnswer: null,
+  isCorrect: false,
 }
 
 const QuizContext = createContext<IQuizContext>({
@@ -64,13 +71,15 @@ export function QuizProvider({ children }: { children: React.ReactElement }) {
       {children}
     </QuizContext.Provider>
   )
-};
+}
 
 export function useQuiz() {
   return useContext(QuizContext);
 }
 
 function QuizReducer(state: QuizState, action: QuizAction): QuizState {
+  const score = state.score;
+
   switch (action.type) {
     case "setQuestion":
       return {...state, question: action.payload};
@@ -78,11 +87,14 @@ function QuizReducer(state: QuizState, action: QuizAction): QuizState {
       return {...state, gameStatus: action.payload};
     case "setUserAnswer":
       return {...state, userAnswer: action.payload};
+    case "setCorrectAnswer":
+      return {...state, correctAnswer: action.payload};
+    case "setIsCorrect":
+      return {...state, isCorrect: action.payload};
     case "setScore":
-      let score = state.score;
-      score[action.payload] += 1;
-
-      return {...state, score: score};
+      return {...state, score: {...score, [action.payload]: score[action.payload] + 1}};
+    case "setCurrentQuestion":
+      return { ...state, currentQuestion: state.currentQuestion > 10 ? 0 : state.currentQuestion + 1 };
     default:
       throw new Error("Unknown action");
   }
